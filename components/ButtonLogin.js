e8c02299a136e7a0b022e53a373247a59fa2df22
karsonthompson <karsonnn22@gmail.com>
2026-01-06 22:02:43 -0500

    Basic sign in and sign out and google auth

---
+++ components/ButtonLogin.js
@@ -0,0 +1,30 @@
+"use client";
+
+import Link from "next/link";
+import { signIn } from "next-auth/react";
+
+const ButtonLogin = ({ session, extraStyle }) => {
+  if (session) {
+    return (
+      <Link
+        href="/dashboard"
+        className={`btn btn-primary ${extraStyle ? extraStyle : ""}`}
+      >
+        Welcome back {session.user.name || "friend"}
+      </Link>
+    );
+  }
+
+  return (
+    <button
+      className={`btn btn-primary ${extraStyle ? extraStyle : ""}`}
+      onClick={() => {
+        signIn(undefined, { callbackUrl: "/dashboard" });
+      }}
+    >
+      Get started
+    </button>
+  );
+};
+
+export default ButtonLogin;

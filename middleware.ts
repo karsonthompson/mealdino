c3aa6f824e09550cb634daf3413ffb724318de2e
karsonthompson <karsonnn22@gmail.com>
2026-01-09 16:59:25 -0500

    Fixing middleware page

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-export default async function middleware(req) {
+export default async function middleware(req: NextRequest) {
   const token = await getToken({ req });
   const { pathname } = req.nextUrl;
 
